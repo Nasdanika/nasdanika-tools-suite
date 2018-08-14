bab4fc34db7933fcace337680049585af483d0cb
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2018-08-14 14:47:11 +1000

    Web Developer Tools

--- install.bat
+++ install.bat
@@ -3,5 +3,7 @@ echo m2e and Tycho configurator
 eclipsec -application org.eclipse.equinox.p2.director -consoleLog -nosplash -repository http://download.eclipse.org/releases/oxygen -installIUs org.eclipse.m2e.feature.feature.group -tag m2e
 eclipsec -application org.eclipse.equinox.p2.director -consoleLog -nosplash -repository http://repo1.maven.org/maven2/.m2e/connectors/m2eclipse-tycho/0.8.1/N/0.8.1.201704211436/ -installIUs org.sonatype.tycho.m2e.feature.feature.group -tag TychoConfigurator
 
+echo Web Developer Tools
+eclipsec -application org.eclipse.equinox.p2.director -consoleLog -nosplash -repository http://download.eclipse.org/releases/photon -installIUs org.eclipse.wst.web_ui.feature.feature.group -tag WebDeveloperTools 
 
 echo Ecore DocGen
