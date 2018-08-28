ef4e639c225d508bd1c3c74545f80d9f3eb61949
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2018-08-28 15:01:33 -0400

    cd target\eclipse.

--- org.nasdanika.tools.repository/install.bat
+++ org.nasdanika.tools.repository/install.bat
@@ -1,2 +1,3 @@
 echo Installing Nasdanika Tools Suite:
+cd target\eclipse
 eclipsec -application org.eclipse.equinox.p2.director -consoleLog -nosplash -repository https://www.nasdanika.org/products/tools/repository -installIUs org.nasdanika.tools.feature.feature.group -tag NasdanikaToolSuite
