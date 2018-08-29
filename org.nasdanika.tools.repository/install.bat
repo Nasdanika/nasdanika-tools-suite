c08445bf895f7c287eb97d42609ebf55b5732e3d
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2018-08-29 17:08:36 -0400

    Fix in binary build.

--- org.nasdanika.tools.repository/install.bat
+++
@@ -1,3 +0,0 @@
-echo Installing Nasdanika Tool Suite:
-cd target\eclipse
-"%JAVA_HOME%\bin\java" -jar plugins/org.eclipse.equinox.launcher_1.5.0.v20180512-1130.jar -application org.eclipse.equinox.p2.director -consoleLog -nosplash -repository https://www.nasdanika.org/products/tools/repository -installIUs org.nasdanika.tools.feature.feature.group -tag NasdanikaToolSuite
